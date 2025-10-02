b80a39411cf88fa90e617122674f156e2915bd71
btigi <btigi@users.noreply.github.com>
2025-10-02 14:35:27 +0100

    Improve LRQ parsing

--- src/ii.DragonPiece/LqrProcessor.cs
+++ src/ii.DragonPiece/LqrProcessor.cs
@@ -2,99 +2,143 @@
 
 namespace ii.DragonPiece
 {
+    public class LqrFile
+    {
+        public List<string> SymbolicNames { get; set; } = new List<string>();
+        public List<LqrFileEntry> FileEntries { get; set; } = new List<LqrFileEntry>();
+    }
+
+    public class LqrFileEntry
+    {
+        public int FileId { get; set; }
+        public string Filename { get; set; } = string.Empty;
+    }
+
     public class LqrProcessor
     {
-        public string[] Process(string filename)
+        public LqrFile Process(string filename)
         {
+            var result = new LqrFile();
+
+            using var fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            using var reader = new BinaryReader(fileStream);
+
+            var signature = reader.ReadBytes(4);
+            var checksum = reader.ReadInt32(); // ?
+            var valueOffset = reader.ReadInt64();
+            var miscDataOffset = reader.ReadInt64();
+            var unknownDataOffset = reader.ReadInt64();
+            var symbolicNameOffset = reader.ReadInt64();
+            var filenameOffset = reader.ReadInt64();
+
+            // -------------------------------------------------------
+            // Values
+            // -------------------------------------------------------
+            reader.BaseStream.Seek(valueOffset + 8, SeekOrigin.Begin);
+            var valueCount = reader.ReadInt32();
+            for (var j = 0; j < valueCount; j++)
+            {
+                var value = reader.ReadInt32();
+            }
+
+            // -------------------------------------------------------
+            // Misc data
+            // -------------------------------------------------------
+            reader.BaseStream.Seek(miscDataOffset, SeekOrigin.Begin);
+            for (var j = 0; j < 8; j++)
+            {
+                var value = reader.ReadInt16();
+            }
+            var textLength = reader.ReadInt32();
+            var textBlock = reader.ReadBytes(textLength * 2);
+            var text1 = Encoding.Unicode.GetString(textBlock );
+            _ = reader.ReadByte();
+            textLength = reader.ReadInt32();
+            textBlock = reader.ReadBytes(textLength * 2);
+            var text2 = Encoding.Unicode.GetString(textBlock);
+
+            // and some unknown data here
+
+            // -------------------------------------------------------
+            // Unknown data
+            // -------------------------------------------------------
+            reader.BaseStream.Seek(unknownDataOffset, SeekOrigin.Begin); 
+
+            // -------------------------------------------------------
+            // Symbolic names
+            // -------------------------------------------------------
+            reader.BaseStream.Seek(symbolicNameOffset, SeekOrigin.Begin);
+            var symbolicNameCount = reader.ReadInt32();
+            var symbolicNameBlockLength = reader.ReadInt32();
+            var symbolicNameBlock = reader.ReadBytes(symbolicNameBlockLength);
+            var symbolicNames = Encoding.Unicode.GetString(symbolicNameBlock);
+            result.SymbolicNames = symbolicNames.Split('\0').ToList();
+
+            // -------------------------------------------------------
+            // Filenames
+            // -------------------------------------------------------
+            reader.BaseStream.Seek(filenameOffset, SeekOrigin.Begin);
+            var entryCount = reader.ReadInt32();
+            var entryCount2 = reader.ReadInt32();
+
+            var i = 0;
             try
             {
-                using var fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
-                using var reader = new BinaryReader(fileStream);
+                var amt = 20;
 
-                var signature = reader.ReadBytes(4);
-                var a = reader.ReadInt32();  // ?
-
-                var b = reader.ReadInt32();  // 40
-                var c = reader.ReadInt32();  // 0
-
-                var d = reader.ReadInt32();  // offset to something2 -> 20 bytes then the name of the associated h2o file (text length, upper case, text length, lower case)?
-                var e = reader.ReadInt32();  // 0
-
-                var f = reader.ReadInt32();  // offset to something3
-                var g = reader.ReadInt32();  // 0
-
-                var h = reader.ReadInt32();  // offset to symbolic names (end of file)
-                var ii = reader.ReadInt32(); // 0 
-
-                var j = reader.ReadInt32();  // 80 / offset to filenames (18 bytes before first text)
-                var k = reader.ReadInt32();  // 0
-
-
-                // Symbolic names ?
-                reader.BaseStream.Seek(h, SeekOrigin.Begin);
-                var count = reader.ReadInt32();
-                var lengthofblock = reader.ReadInt32();
-                var blockbytes = reader.ReadBytes(lengthofblock);
-                var parts = Encoding.Unicode.GetString(blockbytes);
-                var splits = parts.Split('\0');
-
-                // Filenames
-                reader.BaseStream.Seek(j, SeekOrigin.Begin);
-
-                var entryCount = reader.ReadInt32();
-                var entryCount2 = reader.ReadInt32();
-
-                var filenames = new string[entryCount2];
-
-                var i = 0;
-                try
+                if (filename.ToLower().Contains("speechfx") || filename.ToLower().Contains("text") || filename.ToLower().Contains("levels"))
                 {
-                    var amt = 20;
+                    amt = 20;
+                }
+                if (filename.ToLower().Contains("projectiles") || filename.ToLower().Contains("skyboxes"))
+                {
+                    amt = 24;
+                }
+                if (filename.ToLower().Contains("fonts"))
+                {
+                    amt = 16;
+                }
+                if (filename.ToLower().Contains("actors"))
+                {
+                    amt = 28;
+                }
 
-                    if (filename.ToLower().Contains("speechfx") || filename.ToLower().Contains("text") || filename.ToLower().Contains("levels"))
+                for (i = 0; i < entryCount2; i++)
+                {
+                    var fileId = reader.ReadInt32();
+                    var cnt = reader.ReadByte();
+                    if (cnt == 0)
                     {
-                        amt = 20;
-                    }
-                    if (filename.ToLower().Contains("projectiles") || filename.ToLower().Contains("skyboxes"))
-                    {
-                        amt = 24;
-                    }
-                    if (filename.ToLower().Contains("fonts"))
-                    {
-                        amt = 16;
+                        continue;
                     }
 
-                    for (i = 0; i < entryCount2; i++)
+                    if (cnt == 1)
                     {
-                        var fileId = reader.ReadInt32();
-                        //_ = reader.ReadInt16();
-                        var cnt = reader.ReadByte();
-                        if (cnt == 0)
+                        _ = reader.ReadByte();
+                        var length = reader.ReadInt32();
+                        if (length == 0)
                         {
-                            continue;
+                            _ = reader.ReadInt32();
                         }
-
-                        if (cnt == 1)
+                        else
                         {
-                            _ = reader.ReadByte();
-                            var length = reader.ReadInt32();
                             var thisfilename = Encoding.Unicode.GetString(reader.ReadBytes(length * 2)).TrimEnd('\0');
-                            filenames[i] = thisfilename + "_" + Convert.ToString(reader.BaseStream.Position);
+                            var fileEntry = new LqrFileEntry
+                            {
+                                FileId = fileId,
+                                Filename = thisfilename + "_" + Convert.ToString(reader.BaseStream.Position)
+                            };
+                            result.FileEntries.Add(fileEntry);
                             _ = reader.ReadBytes(amt);
                         }
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error reading .lqr file: {ex.Message}");
-                    return null;
-                }
-                return filenames;
             }
             catch (Exception ex)
             {
                 return null;
             }
+            return result;
         }
     }
 }
